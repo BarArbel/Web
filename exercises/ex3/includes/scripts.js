22916e761b76b4d036cbe8a425b70be7da7c475c
bararbel@gmail.com <bararbel@gmail.com>
2018-05-19 13:17:22 +0300

    ex3 commit 8

--- exercises/ex3/includes/scripts.js
+++ exercises/ex3/includes/scripts.js
@@ -1,4 +1,3 @@
-var mainArticleNum = 0;
 var articleOpacity = 0;
 
 function onOver() {
@@ -12,23 +11,20 @@ function onOut() {
 	this.style.opacity = articleOpacity;
 };
 
-function onClick() {
-	if (mainArticleNum++ < 8){
-		var articleObj = document.createElement("article");
-		articleObj.style.opacity = Math.random();
-		articleObj.addEventListener("mouseover", onOver);
-		articleObj.addEventListener("mouseout", onOut);
-		document.getElementsByTagName("main")[0].appendChild(articleObj);
-	}
+function createArticle(name) {
+	var articleObj = document.createElement("article");
+	articleObj.style.width = "250px";
+	articleObj.style.height = "250px";
+	articleObj.style.opacity = Math.random();
+	articleObj.addEventListener("mouseover", onOver);
+	articleObj.addEventListener("mouseout", onOut);
+	document.getElementsByTagName(name)[0].appendChild(articleObj);
 };
 
 function Initialize() {
-	var articleArr = document.getElementsByTagName("article");
-	var imgObj = document.getElementsByTagName("header")[0].getElementsByTagName("section")[0];
+	var imgObj = document.getElementById("pluse");
+	imgObj.addEventListener("click", function(){createArticle("main");});
 	for (var i = 0; i < 4; i++) {
-		articleArr[i].style.opacity = Math.random();
-		articleArr[i].addEventListener("mouseover", onOver);
-		articleArr[i].addEventListener("mouseout", onOut);
+		createArticle("section");
 	}
-	imgObj.addEventListener("click", onClick);
 };
