0b6aeb1ffdf973304ebfbae4a9066dc0dfe97693
bararbel@gmail.com <bararbel@gmail.com>
2018-05-22 14:04:22 +0300

    ex3 commit 10

--- exercises/ex3/includes/scripts.js
+++ exercises/ex3/includes/scripts.js
@@ -4,27 +4,29 @@ function onOver() {
 	articleOpacity = this.style.opacity;
 	this.style.backgroundColor = "#fce100";
 	this.style.opacity = 1;
-};
+}
 
 function onOut() {
 	this.style.backgroundColor = "#000000";
 	this.style.opacity = articleOpacity;
-};
+}
 
-function createArticle(name) {
+function createArticle() {
+	var clearObj = document.getElementsByTagName("main")[0].getElementsByTagName("div")[0];
 	var articleObj = document.createElement("article");
+	articleObj.style.float = "left";
 	articleObj.style.width = "250px";
 	articleObj.style.height = "250px";
 	articleObj.style.opacity = Math.random();
 	articleObj.addEventListener("mouseover", onOver);
 	articleObj.addEventListener("mouseout", onOut);
-	document.getElementsByTagName(name)[0].appendChild(articleObj);
-};
+	document.getElementsByTagName("main")[0].insertBefore(articleObj, clearObj);
+}
 
-function Initialize() {
-	var imgObj = document.getElementById("plus");
-	imgObj.addEventListener("click", function(){createArticle("main");});
+window.onload = function() {
+	var imgObj = document.getElementsByTagName("section")[0];
+	imgObj.addEventListener("click", createArticle);
 	for (var i = 0; i < 4; i++) {
-		createArticle("section");
+		createArticle();
 	}
 };
